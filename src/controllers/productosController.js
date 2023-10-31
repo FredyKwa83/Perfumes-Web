e4f5de06e76c94d2dca90554b565d9bcd5bf13a1
luiscagua <javivy82@hotmail.com>
2023-10-30 22:08:13 -0500

    creacion formularios

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,13 +1,27 @@
-const fs = require("fs");
-const path = require("path");
+// const fs = require("fs");
+// const path = require("path");
 // const productosFilePath = path.join(__dirname, "../data/dataBase.json");
 // const productos = JSON.parse(fs.readFileSync(productosFilePath, 'utf-8'));
 
 const productosController = {
     index:(req,res)=>{
         res.render("./productos/index")
-    }
-    
+    },
 
+    crear:(req,res)=>{
+        res.render("./productos/crearProducto")
+    },
+
+    detalle:(req,res)=>{
+        res.render("./productos/detalleProducto")
+    },
+    
+    editar:(req,res)=>{
+        res.render("./productos/editarProducto")
+    },
+    
+    eliminar:(req,res)=>{
+        res.render("./productos/eliminarProducto")
+    }
 }
 module.exports = productosController;
--- src/routers/productosRouters.js
+++ src/routers/productosRouters.js
@@ -4,7 +4,18 @@ const productosController= require("../controllers/productosController")
   
 //crear ruta raiz
 router.get("/", productosController.index)
+
 router.get("/crear", productosController.crear)
+
+router.get("/detalle", productosController.detalle)
+
+router.get("/editar", productosController.editar)
+
+router.get("/eliminar", productosController.eliminar)
+
+
+
+
   
 
 
